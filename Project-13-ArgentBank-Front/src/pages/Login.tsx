c703450a5cae7674b505fe51ae32374af380b630
hichamecode <hichame.code@gmail.com>
2024-08-25 22:37:37 +0200

    install of react hook form + form frontend validation

--- Project-13-ArgentBank-Front/src/pages/Login.tsx
+++ Project-13-ArgentBank-Front/src/pages/Login.tsx
@@ -1,8 +1,26 @@
 import './Login.scss'
 import Layout from '../components/Layout';
-import { Link } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+
 
 function Login() {
+
+      const navigate = useNavigate();
+      const handleSub = (e: React.FormEvent) => {
+            e.preventDefault();
+            navigate('/login/profile');
+      }
+
+      type FormFields = {
+            email: string;
+            password: string;
+      }
+
+      const { register, handleSubmit, formState: { errors } } = useForm<FormFields>();
+      const onSubmit: SubmitHandler<FormFields> = data => console.log(data);
+
 
       return (
             <Layout>
@@ -11,21 +29,28 @@ function Login() {
                         <section className="sign-in-content">
                               <i className="fa fa-user-circle sign-in-icon"></i>
                               <h1 className='sign-in-title'>Sign In</h1>
-                              <form>
+                              <form onSubmit={handleSubmit(onSubmit)}>
                                     <div className="input-wrapper">
-                                          <label htmlFor="username">Username</label
-                                          ><input type="text" id="username" />
+                                          <label htmlFor="username" >Username</label>
+                                          <input {...register("email", {
+                                                required: true,
+                                                pattern: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/
+                                          })} type="text" id="username" />
+                                          {errors.email && <span className='error-text'>the username has an invalid format</span>}
                                     </div>
                                     <div className="input-wrapper">
                                           <label htmlFor="password">Password</label
-                                          ><input type="password" id="password" />
+                                          ><input {...register("password", {
+                                                required: true,
+                                                minLength: 6
+                                          })}  type="password" id="password" />
+                                          {errors.password && <span className='error-text'>the password must have at least 6 characters</span>}
                                     </div>
                                     <div className="input-remember">
                                           <input type="checkbox" id="remember-me" /><label htmlFor="remember-me"
-                                          >Remember me</label
-                                          >
+                                          >Remember me</label>
                                     </div>
-                                    <Link to="./profile" className="sign-in-button">Sign In</Link>
+                                    <button type='submit' className="sign-in-button">Sign In</button>
                               </form>
                         </section>
 
@@ -34,7 +59,6 @@ function Login() {
 
       )
 }
-
 
 
 export default Login;
